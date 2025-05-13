a50a3dfcd1880a28bb920ffa7eb0a04445c95cff
shreyanshrathred <shreyanshrathred@gmail.com>
2025-05-12 22:51:06 -0500

    feat(web): update landing page hero video

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -93,7 +93,7 @@ export default function LandingPage() {
             playsInline // Important for iOS
             className="absolute top-0 left-0 w-full h-full object-cover z-0 opacity-30" // Adjust opacity as needed
           >
-            <source src="/videos/landingpagebackground.mp4" type="video/mp4" />
+            <source src="/videos/heropagebackground.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           
