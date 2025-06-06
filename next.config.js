ab092f5deebb0b11675b6ccc1223073f3fc6fcff
shreyanshrathred <shreyanshrathred@gmail.com>
2025-06-06 16:44:00 -0500

    feat: configure Tamagui and fix build issues

--- next.config.js
+++ next.config.js
@@ -1,53 +1,124 @@
-// This file will be used to modify the Next.js config for a minimal deployment
+const { withTamagui } = require('@tamagui/next-plugin')
+const { join } = require('path')
 
-module.exports = {
-  output: 'standalone',
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  webpack: (config, { isServer }) => {
-    // Add a rule to handle .node files for Supabase
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: 'node-loader',
-      options: {
-        name: '[name].[ext]',
-      },
-    });
+const boolVals = {
+  true: true,
+  false: false,
+}
 
-    // Ignore optional native dependencies of ws
-    if (!isServer) {
+const disableExtraction =
+  boolVals[process.env.DISABLE_EXTRACTION] ?? process.env.NODE_ENV === 'development'
+
+const plugins = [
+  withTamagui({
+    config: './tamagui.config.ts',
+    components: ['tamagui', '@research-collab/ui'],
+    importsWhitelist: ['constants.js', 'colors.js'],
+    outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
+    logTimings: true,
+    disableExtraction,
+    // experiment - Fix subpaths resolving
+    shouldExtract: (path) => {
+      if (path.includes(join('packages', 'app'))) {
+        return true
+      }
+    },
+  }),
+]
+
+module.exports = function () {
+  /** @type {import('next').NextConfig} */
+  let config = {
+    //
+    // Europe (Frankfurt)
+    // i18n: {
+    //   locales: ['en-US', 'es-ES', 'fr-FR', 'de-DE'],
+    //   defaultLocale: 'en-US',
+    // },
+    // output: 'standalone',
+    //
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'gravatar.com',
+          port: '',
+          pathname: '/avatar/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com',
+          port: '',
+          pathname: '/a/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.pravatar.cc',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'plus.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+    //
+    //
+    transpilePackages: [
+      'solito',
+      'react-native-web',
+      'expo-linking',
+      'expo-constants',
+      'expo-modules-core',
+      'expo-image-picker',
+      'expo-av',
+      '@research-collab/ui',
+    ],
+    //
+    //
+    experimental: {
+      // optimizeCss: true,
+      // scrollRestoration: true,
+      // legacyBrowsers: false,
+      // serverActions: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    // modularizeImports: {
+    //   '@tamagui/lucide-icons': {
+    //     transform: `@tamagui/lucide-icons/dist/esm/icons/{{kebabCase member}}`,
+    //     skipDefaultConversion: true,
+    //   },
+    // },
+    //
+    //
+    //
+    webpack: (config, { isServer, dev, buildId, config: { pageExtensions } }) => {
       config.resolve.alias = {
         ...config.resolve.alias,
-        bufferutil: false,
-        'utf-8-validate': false,
-      };
-    }
-    return config;
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/',
-      },
-      {
-        source: '/todo',
-        destination: '/todo',
+        'react-native$': 'react-native-web',
       }
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: '/((?!todo|api|_next/static|_next/image|favicon.ico).*)',
-        destination: '/404',
-        permanent: false,
-      },
-    ];
+      return config
+    },
+    //
   }
-}; 
+
+  for (const plugin of plugins) {
+    config = {
+      ...config,
+      ...plugin(config),
+    }
+  }
+
+  return config
+} 
