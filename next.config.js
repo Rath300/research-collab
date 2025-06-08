7e3b1aed20e036d6541d2123c908726a2ffc09bc
shreyanshrathred <shreyanshrathred@gmail.com>
2025-06-08 16:41:03 -0500

    fix: configure tamagui next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,32 +1,61 @@
-// This file will be used to modify the Next.js config for a minimal deployment
+const { withTamagui } = require('@tamagui/next-plugin');
+const { join } = require('path');
 
-module.exports = {
-  output: 'standalone',
-  swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  webpack: (config, { isServer }) => {
-    // Add a rule to handle .node files for Supabase
-    config.module.rules.push({
-      test: /\.node$/,
-      loader: 'node-loader',
-      options: {
-        name: '[name].[ext]',
+const bools = {
+  // if you want to have a slower build with more descriptive server-side rendered classnames
+  shouldAddDebugShorthands: true,
+  // this adds proper animations support (slower load)
+  shouldDisableAnimationDriver: false,
+};
+
+const tamaguiPlugin = withTamagui({
+  config: './tamagui.config.ts',
+  components: ['tamagui', 'ui'],
+  importsWhitelist: ['constants.js', 'colors.js'],
+  outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
+  logTimings: true,
+  debug: false,
+  ...bools,
+});
+
+module.exports = function (name, { defaultConfig }) {
+  let config = {
+    ...defaultConfig,
+    ...{
+      // This file will be used to modify the Next.js config for a minimal deployment
+      output: 'standalone',
+      swcMinify: true,
+      eslint: {
+        ignoreDuringBuilds: true,
       },
-    });
+      typescript: {
+        ignoreBuildErrors: true,
+      },
+      webpack: (config, { isServer }) => {
+        // Add a rule to handle .node files for Supabase
+        config.module.rules.push({
+          test: /\.node$/,
+          loader: 'node-loader',
+          options: {
+            name: '[name].[ext]',
+          },
+        });
 
-    // Ignore optional native dependencies of ws
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        bufferutil: false,
-        'utf-8-validate': false,
-      };
-    }
-    return config;
-  },
+        // Ignore optional native dependencies of ws
+        if (!isServer) {
+          config.resolve.alias = {
+            ...config.resolve.alias,
+            bufferutil: false,
+            'utf-8-validate': false,
+          };
+        }
+        return config;
+      },
+    },
+  };
+
+  // Enable Tamagui plugin
+  config = { ...config, ...tamaguiPlugin(config) };
+
+  return config;
 }; 
