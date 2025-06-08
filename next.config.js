74254d4b05392be70af008e75d0490777fda4f38
shreyanshrathred <shreyanshrathred@gmail.com>
2025-06-08 16:49:53 -0500

    fix: correctly configure tamagui and next.js plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 const { withTamagui } = require('@tamagui/next-plugin');
-const { join } = require('path');
-
-const bools = {
-  // if you want to have a slower build with more descriptive server-side rendered classnames
-  shouldAddDebugShorthands: true,
-  // this adds proper animations support (slower load)
-  shouldDisableAnimationDriver: false,
-};
 
 const tamaguiPlugin = withTamagui({
   config: './tamagui.config.ts',
@@ -15,47 +7,37 @@ const tamaguiPlugin = withTamagui({
   outputCSS: process.env.NODE_ENV === 'production' ? './public/tamagui.css' : null,
   logTimings: true,
   debug: false,
-  ...bools,
+  shouldAddDebugShorthands: true,
+  shouldDisableAnimationDriver: false,
 });
 
-module.exports = function (name, { defaultConfig }) {
-  let config = {
-    ...defaultConfig,
-    ...{
-      // This file will be used to modify the Next.js config for a minimal deployment
-      output: 'standalone',
-      swcMinify: true,
-      eslint: {
-        ignoreDuringBuilds: true,
+const nextConfig = {
+  output: 'standalone',
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: 'node-loader',
+      options: {
+        name: '[name].[ext]',
       },
-      typescript: {
-        ignoreBuildErrors: true,
-      },
-      webpack: (config, { isServer }) => {
-        // Add a rule to handle .node files for Supabase
-        config.module.rules.push({
-          test: /\.node$/,
-          loader: 'node-loader',
-          options: {
-            name: '[name].[ext]',
-          },
-        });
+    });
 
-        // Ignore optional native dependencies of ws
-        if (!isServer) {
-          config.resolve.alias = {
-            ...config.resolve.alias,
-            bufferutil: false,
-            'utf-8-validate': false,
-          };
-        }
-        return config;
-      },
-    },
-  };
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        bufferutil: false,
+        'utf-8-validate': false,
+      };
+    }
+    return config;
+  },
+};
 
-  // Enable Tamagui plugin
-  config = { ...config, ...tamaguiPlugin(config) };
-
-  return config;
-}; 
+module.exports = tamaguiPlugin(nextConfig);
---
+++ tamagui.config.ts
@@ -0,0 +1,12 @@
+import { createTamagui } from 'tamagui'
+import { config } from '@tamagui/config/v2'
+
+const tamaguiConfig = createTamagui(config)
+
+export type AppConfig = typeof tamaguiConfig
+
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends AppConfig {}
+}
+
+export default tamaguiConfig 
